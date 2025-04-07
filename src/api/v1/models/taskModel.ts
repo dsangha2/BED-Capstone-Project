fe82d90c6b9d26e3b3c72278309035560b4b1180
Diljot Singh Sangha <dsangha2@academic.rrc.ca>
2025-04-06 20:44:04 -0500

    feat: implemented task model

---
+++ src/api/v1/models/taskModel.ts
@@ -0,0 +1,9 @@
+export interface Task {
+    id: string;
+    projectId: string;
+    title: string;
+    description: string;
+    status: "pending" | "in-progress" | "completed";
+    createdAt: Date;
+    updatedAt: Date;
+  }
